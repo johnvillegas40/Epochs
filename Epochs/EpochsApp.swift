2a9e6ddf252fb6a1be83e765ec3462e5697914e5
Johnny Villegas <johnnywvillegas@icloud.com>
2025-08-29 15:29:47 -0500

    Initial Commit

---
+++ Epochs/EpochsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EpochsApp.swift
+//  Epochs
+//
+//  Created by Johnny Villegas on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct EpochsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EpochsTests/EpochsTests.swift
@@ -0,0 +1,17 @@
+//
+//  EpochsTests.swift
+//  EpochsTests
+//
+//  Created by Johnny Villegas on 8/29/25.
+//
+
+import Testing
+@testable import Epochs
+
+struct EpochsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
